3337f4573876cb1e9a90158876ab7369843af4dc
TAzisK <tarreq.azis20@gmail.com>
2021-09-27 20:58:18 +0700

    first commit

---
+++ application/controllers/introduction.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class introduction extends CI_Controller {
+    public function kenal()
+    {
+        echo "PERKENALKAN";
+        echo "</br>";
+        echo "Perkenalkan nama saya Az.. Saya tinggal di Depok";
+    }
+
+}
